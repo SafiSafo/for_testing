747e2edb388852fca1b2975adcd0f5af2d52ec9c
SafiSafo <sophiameskine@gmail.com>
2016-10-31 18:14:54 +0100

    First commit

---
+++ my_appp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_appp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Life is awesome");
+        }
+    }
+}
